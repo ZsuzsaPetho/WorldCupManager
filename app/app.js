b19b88f3ddf781b3c61627f7a60242e8e34137d3
ZsuzsaPetho <zsuzsapethocodecool@gmail.com>
2018-07-17 10:12:28 +0200

    serviceWorker experimenting

--- app/app.js
+++ app/app.js
@@ -50,7 +50,16 @@ myApp.directive( 'goClick', function ( $location ) {
     };
 });
 
-
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/serviceWorker/serviceWorker.js')
+        .then(function(reg) {
+            // registration worked
+            console.log('Registration succeeded. Scope is ' + reg.scope);
+        }).catch(function(error) {
+        // registration failed
+        console.log('Registration failed with ' + error);
+    });
+}
 
 
 
